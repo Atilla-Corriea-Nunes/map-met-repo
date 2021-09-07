7e368da826d90811e1ccdb59ca66e671e136a808
Atilla <you@example.com>
2021-09-07 12:11:47 +0200

    ik heb feestlunch en speelhal uitgerekend en erbij gedaan

---
+++ de speelhal-dag.py
@@ -0,0 +1,11 @@
+num1 = 7.45
+num2 = 135
+num3 = 0.37
+
+
+sum1 = num2 / 5
+sum2 = sum1 * num3
+sum4 = num1 * 3
+sum3 = sum2 + sum4
+
+print('de kosten zullen '+ str(sum3) +' zijn')
---
+++ feestlunch.py
@@ -0,0 +1,10 @@
+num1 = 17
+num2 = 0.39
+num3 = 2.78
+num4 = 2
+
+sum1 = num1 * num2
+sum2 = num3 * num4
+sum3 = sum1 + sum2
+
+print('je moet '+ str(sum3) +' betalen')
